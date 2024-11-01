a60ae29985865723842c132992d7dedc761a2456
Ana Novković <anaiscoding@gmail.com>
2024-11-01 11:13:52 +0100

    Add logic to index.html, add logo.jpeg

---
+++ includes/db.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost';
+$db = 'apartment_booking';
+$user = 'root';
+$pass = '';
+
+try {
+    $db = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
